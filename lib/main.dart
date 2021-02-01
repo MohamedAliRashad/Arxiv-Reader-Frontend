774b5842f0105e48fceeffb35a2e8ece52dcd41d
MohamedAliRashad <m.rashadnow@gmail.com>
2021-02-02 00:19:08 +0200

    Initial Version for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,214 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  var _categories = [
+    'Accelerator Physics',
+    'Adaptation and Self-Organizing Systems',
+    'Algebraic Geometry',
+    'Algebraic Topology',
+    'Analysis of PDEs',
+    'Applications',
+    'Applied Physics',
+    'Artificial Intelligence',
+    'Astrophysics',
+    'Astrophysics of Galaxies',
+    'Atmospheric and Oceanic Physics',
+    'Atomic Physics',
+    'Atomic and Molecular Clusters',
+    'Audio and Speech Processing',
+    'Biological Physics',
+    'Biomolecules',
+    'Category Theory',
+    'Cell Behavior',
+    'Cellular Automata and Lattice Gases',
+    'Chaotic Dynamics',
+    'Chemical Physics',
+    'Classical Analysis and ODEs',
+    'Classical Physics',
+    'Combinatorics',
+    'Commutative Algebra',
+    'Complex Variables',
+    'Computation',
+    'Computation and Language',
+    'Computational Complexity',
+    'Computational Engineering, Finance, and Science',
+    'Computational Finance',
+    'Computational Geometry',
+    'Computational Physics',
+    'Computer Science and Game Theory',
+    'Computer Vision and Pattern Recognition',
+    'Computers and Society',
+    'Cosmology and Nongalactic Astrophysics',
+    'Cryptography and Security',
+    'Data Analysis, Statistics and Probability',
+    'Data Structures and Algorithms',
+    'Databases',
+    'Differential Geometry',
+    'Digital Libraries',
+    'Discrete Mathematics',
+    'Disordered Systems and Neural Networks',
+    'Distributed, Parallel, and Cluster Computing',
+    'Dynamical Systems',
+    'Earth and Planetary Astrophysics',
+    'Econometrics',
+    'Economics',
+    'Emerging Technologies',
+    'Exactly Solvable and Integrable Systems',
+    'Fluid Dynamics',
+    'Formal Languages and Automata Theory',
+    'Functional Analysis',
+    'General Finance',
+    'General Literature',
+    'General Mathematics',
+    'General Physics',
+    'General Relativity and Quantum Cosmology',
+    'General Topology',
+    'Genomics',
+    'Geometric Topology',
+    'Geophysics',
+    'Graphics',
+    'Group Theory',
+    'Hardware Architecture',
+    'High Energy Astrophysical Phenomena',
+    'High Energy Physics - Experiment',
+    'High Energy Physics - Lattice',
+    'High Energy Physics - Phenomenology',
+    'High Energy Physics - Theory',
+    'History and Overview',
+    'History and Philosophy of Physics',
+    'Human-Computer Interaction',
+    'Image and Video Processing',
+    'Information Retrieval',
+    'Information Theory',
+    'Instrumentation and Detectors',
+    'Instrumentation and Methods for Astrophysics',
+    'K-Theory and Homology',
+    'Learning',
+    'Logic',
+    'Logic in Computer Science',
+    'Machine Learning',
+    'Materials Science',
+    'Mathematical Finance',
+    'Mathematical Physics',
+    'Mathematical Software',
+    'Medical Physics',
+    'Mesoscale and Nanoscale Physics',
+    'Methodology',
+    'Metric Geometry',
+    'Molecular Networks',
+    'Multiagent Systems',
+    'Multimedia',
+    'Networking and Internet Architecture',
+    'Neural and Evolutionary Computing',
+    'Neurons and Cognition',
+    'Nuclear Experiment',
+    'Nuclear Theory',
+    'Number Theory',
+    'Numerical Analysis',
+    'Operating Systems',
+    'Operator Algebras',
+    'Optics',
+    'Optimization and Control',
+    'Other Computer Science',
+    'Other Condensed Matter',
+    'Other Quantitative Biology',
+    'Other Statistics',
+    'Pattern Formation and Solitons',
+    'Performance',
+    'Physics Education',
+    'Physics and Society',
+    'Plasma Physics',
+    'Popular Physics',
+    'Populations and Evolution',
+    'Portfolio Management',
+    'Pricing of Securities',
+    'Probability',
+    'Programming Languages',
+    'Quantitative Methods',
+    'Quantum Algebra',
+    'Quantum Gases',
+    'Quantum Physics',
+    'Representation Theory',
+    'Rings and Algebras',
+    'Risk Management',
+    'Robotics',
+    'Signal Processing',
+    'Social and Information Networks',
+    'Soft Condensed Matter',
+    'Software Engineering',
+    'Solar and Stellar Astrophysics',
+    'Sound',
+    'Space Physics',
+    'Spectral Theory',
+    'Statistical Finance',
+    'Statistical Mechanics',
+    'Statistics Theory',
+    'Strongly Correlated Electrons',
+    'Subcellular Processes',
+    'Superconductivity',
+    'Symbolic Computation',
+    'Symplectic Geometry',
+    'Systems and Control',
+    'Tissues and Organs',
+    'Trading and Market Microstructure'
+  ];
+  int _categoriesIndex = 0;
+  var rng = new Random();
+  void _categoriesIncrement() {
+    _categoriesIndex = rng.nextInt(_categories.length);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          title: Text(
+            "Arxiv Reader",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.redAccent, fontSize: 25),
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            // padding: EdgeInsets.only(left: 20.0),
+            icon: Icon(Icons.arrow_back_ios_sharp),
+            onPressed: () => print("menu"),
+            iconSize: 20,
+            color: Colors.black,
+          ),
+          actions: [
+            IconButton(
+              // padding: EdgeInsets.only(right: 25.0),
+              icon: Icon(Icons.search_sharp),
+              onPressed: () => print("search"),
+              iconSize: 30,
+              color: Colors.black,
+            )
           ],
         ),
+        body: Center(
+          child: AnimatedOpacity(
+            duration: Duration(milliseconds: 100),
+            opacity: 1.0,
+            child: Text(
+              _categories[_categoriesIndex],
+              style: TextStyle(
+                  fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red),
+            ),
+          ),
+          heightFactor: 25,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _categoriesIncrement,
+          child: Icon(Icons.flip),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add_a_photo),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
